6fc21312ee4fce3f620a09d051314810cd593685
wrabiega <78311320+wrabiega@users.noreply.github.com>
2021-01-31 18:59:14 +0100

    Create Demo3.java
    File 3 created remotly.

---
+++ Demo3.java
@@ -0,0 +1,11 @@
+  
+class Demo3
+{
+public static void main(String args[])
+{
+	System.out.println("Java Demo Class 1");
+	System.out.println("This is second line of demo class 1");
+	System.out.println("This is second line of demo class 2");
+  System.out.println("This is second line of demo class 3");
+}
+}
