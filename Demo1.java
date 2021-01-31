8691f5dbc21edf193b1b10aadbe4dc21a619e06d
wrabiega <78311320+wrabiega@users.noreply.github.com>
2021-01-31 18:58:19 +0100

    Update Demo1.java
    Changed remotly

--- Demo1.java
+++ Demo1.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 {
 	System.out.println("Java Demo Class 1");
 	System.out.println("This is second line of demo class 1");
+	System.out.println("This is second line of demo class 2");
 }
 }
