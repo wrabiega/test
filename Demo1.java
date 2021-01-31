a1d880cd47cc5d737b765914b22d761c07b52e2f
wrabiega <wojciech.rabiega@accessholding.com>
2021-01-31 17:00:46 +0000

    Change to Demo1 and two new files

--- Demo1.java
+++ Demo1.java
@@ -3,5 +3,6 @@ class Demo1
 public static void main(String args[])
 {
 	System.out.println("Java Demo Class 1");
+	System.out.println("This is second line of demo class 1");
 }
 }
---
+++ Demo2.java
@@ -0,0 +1,7 @@
+class Demo2
+{
+public static void main(String args[])
+{
+	System.out.println("Java Demo Class 2");
+}
+}
